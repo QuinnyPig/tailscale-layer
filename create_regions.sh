37cdbccf4ef137a32f94c401ca375d7273e2c53c
Corey Quinn <QuinnyPig@users.noreply.github.com>
2021-07-07 17:53:26 -0700

    Create region script

---
+++ create_regions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+REGIONS=$(aws ec2 describe-regions \
+    --all-regions \
+    --query "Regions[].{Name:RegionName}" \
+    --output text)
+    
+cat << EOF > ./regions.sh
+REGIONS = {
+  $REGIONS
+}
+EOF
+chmod +x regions.sh
